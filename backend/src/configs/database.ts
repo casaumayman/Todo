b5154ff7ca77a07859f0931c840fda137fbfe150
casaumayman <casaumayman1@gmail.com>
2023-02-01 22:53:54 +0700

    done pass env file

--- backend/src/configs/database.ts
+++ backend/src/configs/database.ts
@@ -2,10 +2,10 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const dbConfigs: TypeOrmModuleOptions = {
     type: 'mysql',
-    host: 'localhost',
+    host: 'database',
     port: 3306,
-    username: 'casau',
-    password: 'anhyeuem',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: 'todo',
     synchronize: true,
     autoLoadEntities: true
